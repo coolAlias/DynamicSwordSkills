3c659dd900907162e584848317650c669cf46564
coolAlias <BASbliznet@gmail.com>
2015-01-29 18:41:35 -0800

    Clearly mark client config settings

--- src/main/java/dynamicswordskills/ref/Config.java
+++ src/main/java/dynamicswordskills/ref/Config.java
@@ -28,27 +28,28 @@ import dynamicswordskills.skills.SkillBase;
 
 public class Config
 {
-	/*================== GENERAL =====================*/
-	/** Whether to use default movement controls to activate skills such as Dodge */
+	/*================== CLIENT SIDE SETTINGS =====================*/
+	/** [Combo HUD] Whether the combo hit counter will display by default (may be toggled in game) */
+	private static boolean enableComboHud;
+	/** [Combo HUD] Number of combo hits to display */
+	private static int hitsToDisplay;
+	/** [Controls] Whether to use vanilla movement keys to activate skills such as Dodge and Parry */
 	private static boolean allowVanillaControls;
+	/** [Controls] Whether Dodge and Parry require double-tap or not (double-tap always required with vanilla control scheme) */
+	private static boolean doubleTap;
+	/** [Targeting] Whether auto-targeting is enabled or not (toggle in game by pressing '.') */
+	private static boolean autoTarget;
+	/** [Targeting] Whether players can be targeted (toggle in game by pressing '.' while sneaking) */
+	private static boolean enablePlayerTarget;
+	/*================== GENERAL =====================*/
 	/** Default swing speed (anti-left-click-spam): Sets base number of ticks between each left-click (0 to disable)[0-20] */
 	private static int baseSwingSpeed;
-	/** Whether Dodge and Parry require double-tap or not */
-	private static boolean doubleTap;
-	/** Whether auto-targeting is enabled or not */
-	private static boolean autoTarget;
-	/** Whether players can be targeted */
-	private static boolean enablePlayerTarget;
 	/** Whether all players should start with a Basic Skill orb */
 	private static boolean enableBonusOrb;
 	/** Weight for skill orbs when added to vanilla chest loot (0 to disable) [0-10] */
 	private static int chestLootWeight;
 	/** [Back Slice] Allow Back Slice to potentially knock off player armor */
 	private static boolean allowDisarmorPlayer;
-	/** [Combo HUD] Max hits to display */
-	private static int hitsToDisplay;
-	/** [Combo HUD] Whether the combo hit counter will display by default (may be toggled in game) */
-	private static boolean enableComboHud;
 	/** [Parry] Bonus to disarm based on timing: tenths of a percent added per tick remaining on the timer [0-50] */
 	private static float disarmTimingBonus;
 	/** [Parry] Penalty to disarm chance: percent per Parry level of the opponent, default negates defender's skill bonus so disarm is based entirely on timing [0-20] */
@@ -80,26 +81,28 @@ public class Config
 	public static void init(FMLPreInitializationEvent event) {
 		Configuration config = new Configuration(new File(event.getModConfigurationDirectory().getAbsolutePath() + ModInfo.CONFIG_PATH));
 		config.load();
-		
+		/*================== CLIENT SIDE SETTINGS =====================*/
+		String category = "client";
+		config.addCustomCategoryComment(category, "This category contains client side settings; i.e. they are not synchronized with the server.");
+		enableComboHud = config.get(category, "[Combo HUD] Whether the combo hit counter will display by default (toggle in game: 'v')", true).getBoolean(true);
+		hitsToDisplay = MathHelper.clamp_int(config.get(category, "[Combo HUD] Max hits to display in Combo HUD [0-12]", 3).getInt(), 0, 12);
+		allowVanillaControls = config.get(category, "[Controls] Whether to use vanilla movement keys to activate skills such as Dodge and Parry", true).getBoolean(true);
+		doubleTap = config.get(category, "[Controls] Whether Dodge and Parry require double-tap or not (double-tap always required with vanilla control scheme)", true).getBoolean(true);
+		autoTarget = config.get(category, "[Targeting] Whether auto-targeting is enabled or not (toggle in game: '.')", true).getBoolean(true);
+		enablePlayerTarget = config.get(category, "[Targeting] Whether players can be targeted (toggle in game: '.' while sneaking)", true).getBoolean(true);
 		/*================== GENERAL =====================*/
-		allowVanillaControls = config.get("general", "Allow vanilla controls to activate skills", true).getBoolean(true);
-		autoTarget = config.get("general", "Enable auto-targeting of next opponent", true).getBoolean(true);
 		baseSwingSpeed = MathHelper.clamp_int(config.get("general", "Default swing speed (anti-left-click-spam): Sets base number of ticks between each left-click (0 to disable)[0-20]", 0).getInt(), 0, 20);
-		enablePlayerTarget = config.get("general", "Enable targeting of players by default (can be toggled in game)", true).getBoolean(true);
-		doubleTap = config.get("general", "Require double tap activation", true).getBoolean(true);
 		enableBonusOrb = config.get("general", "Whether all players should start with a Basic Skill orb", true).getBoolean(true);
 		chestLootWeight = MathHelper.clamp_int(config.get("general", "Weight for skill orbs when added to vanilla chest loot (0 to disable) [0-10]", 1).getInt(), 0, 10);
 		allowDisarmorPlayer = config.get("general", "[Back Slice] Allow Back Slice to potentially knock off player armor", true).getBoolean(true);
-		hitsToDisplay = Math.max(0, config.get("general", "[Combo HUD] Max hits to display [0-12]", 3).getInt());
-		enableComboHud = config.get("general", "[Combo HUD] Whether the combo hit counter will display by default (may be toggled in game)", true).getBoolean(true);
 		disarmTimingBonus = 0.001F * (float) MathHelper.clamp_int(config.get("general", "[Parry] Bonus to disarm based on timing: tenths of a percent added per tick remaining on the timer [0-50]", 25).getInt(), 0, 50);
 		disarmPenalty = 0.01F * (float) MathHelper.clamp_int(config.get("general", "[Parry] Penalty to disarm chance: percent per Parry level of the opponent, default negates defender's skill bonus so disarm is based entirely on timing [0-20]", 10).getInt(), 0, 20);
 		enableRandomSkillSwords = config.get("general", "[Skill Swords] Enable randomized Skill Swords to appear as loot in various chests", true).getBoolean(true);
 		enableCreativeSkillSwords = config.get("general", "[Skill Swords] Enable Skill Swords in the Creative Tab (iron only, as examples)", true).getBoolean(true);
 		skillSwordLevel = MathHelper.clamp_int(config.get("general", "[Skill Swords] Skill level provided by the Creative Tab Skill Swords [1-5]", 3).getInt(), 1, 5);
 		requireFullHealth = config.get("general", "[Super Spin Attack | Sword Beam] True to require a completely full health bar to use, or false to allow a small amount to be missing per level", false).getBoolean(false);
-		
-		String category = "enabledskills";
+
+		category = "enabledskills";
 		config.addCustomCategoryComment(category,
 				"Disabling a skill prevents players from learning or using that skill, but does not change the player\'s known skills."
 				+ "\nSkill items previously generated as loot may be found but not used, and subsequent loot will not generate with that skill."
@@ -122,18 +125,19 @@ public class Config
 		}
 		config.save();
 	}
-	/*================== SKILLS =====================*/
-	public static boolean giveBonusOrb() { return enableBonusOrb; }
-	public static int getLootWeight() { return chestLootWeight; }
+	/*================== CLIENT SIDE SETTINGS =====================*/
+	public static boolean isComboHudEnabled() { return enableComboHud; }
+	public static int getHitsToDisplay() { return hitsToDisplay; }
 	public static boolean allowVanillaControls() { return allowVanillaControls; }
-	public static int getBaseSwingSpeed() { return baseSwingSpeed; }
 	public static boolean requiresDoubleTap() { return doubleTap; }
 	public static boolean autoTargetEnabled() { return autoTarget; }
 	public static boolean toggleAutoTarget() { autoTarget = !autoTarget; return autoTarget; }
 	public static boolean canTargetPlayers() { return enablePlayerTarget; }
 	public static boolean toggleTargetPlayers() { enablePlayerTarget = !enablePlayerTarget; return enablePlayerTarget; }
-	public static int getHitsToDisplay() { return hitsToDisplay; }
-	public static boolean isComboHudEnabled() { return enableComboHud; }
+	/*================== SKILLS =====================*/
+	public static boolean giveBonusOrb() { return enableBonusOrb; }
+	public static int getLootWeight() { return chestLootWeight; }
+	public static int getBaseSwingSpeed() { return baseSwingSpeed; }
 	public static boolean areRandomSwordsEnabled() { return enableRandomSkillSwords; }
 	public static boolean areCreativeSwordsEnabled() { return enableCreativeSkillSwords; }
 	public static boolean canDisarmorPlayers() { return allowDisarmorPlayer; }
