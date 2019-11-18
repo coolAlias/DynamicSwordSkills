bed97fc7296576d8f57c6c865bad5e455a6f364c
coolAlias <BASbliznet@gmail.com>
2019-11-17 22:23:39 -0800

    Removed key bindings made redundant by in-game config settings
    - Key used to toggle the Combo HUD
    
    - Key used to toggle auto-targeting / target players / target passive mobs

--- src/main/java/dynamicswordskills/client/DSSKeyHandler.java
+++ src/main/java/dynamicswordskills/client/DSSKeyHandler.java
@@ -17,10 +17,6 @@
 
 package dynamicswordskills.client;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.settings.KeyBinding;
-import net.minecraft.util.StatCollector;
-
 import org.lwjgl.input.Keyboard;
 
 import cpw.mods.fml.client.registry.ClientRegistry;
@@ -36,7 +32,9 @@ import dynamicswordskills.network.server.OpenGuiPacket;
 import dynamicswordskills.ref.Config;
 import dynamicswordskills.skills.ILockOnTarget;
 import dynamicswordskills.skills.SkillBase;
-import dynamicswordskills.util.PlayerUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.util.StatCollector;
 
 @SideOnly(Side.CLIENT)
 public class DSSKeyHandler
@@ -52,9 +50,7 @@ public class DSSKeyHandler
 	KEY_RIGHT = 4,
 	KEY_DOWN = 5,
 	KEY_BLOCK = 6,
-	KEY_TOGGLE_AUTOTARGET = 7,
-	KEY_SKILLS_GUI = 8,
-	KEY_TOGGLE_HUD = 9;
+	KEY_SKILLS_GUI = 7;
 
 	/** Key descriptions - this is what the player sees when changing key bindings in-game */
 	public static final String[] desc = {
@@ -65,9 +61,7 @@ public class DSSKeyHandler
 			"right",
 			"down",
 			"block",
-			"toggleat",
-			"skills_gui",
-			"togglehud"
+			"skills_gui"
 	};
 
 	/** Default key values */
@@ -79,9 +73,7 @@ public class DSSKeyHandler
 			Keyboard.KEY_RIGHT,
 			Keyboard.KEY_DOWN,
 			Keyboard.KEY_RCONTROL,
-			Keyboard.KEY_PERIOD,
-			Keyboard.KEY_P,
-			Keyboard.KEY_V
+			Keyboard.KEY_P
 	};
 
 	public static final KeyBinding[] keys = new KeyBinding[desc.length];
@@ -115,19 +107,8 @@ public class DSSKeyHandler
 				if (skills.hasSkill(SkillBase.swordBasic)) {
 					PacketDispatcher.sendToServer(new ActivateSkillPacket(SkillBase.swordBasic));
 				}
-			} else if (kb == keys[KEY_TOGGLE_AUTOTARGET].getKeyCode()) {
-				if (mc.thePlayer.isSneaking()) {
-					PlayerUtils.sendTranslatedChat(mc.thePlayer, "key.dss.toggletp", StatCollector.translateToLocal(Config.toggleTargetPlayers() ? "key.dss.enable" : "key.dss.disable"));
-				} else if (mc.thePlayer.isSprinting()) {
-					PlayerUtils.sendTranslatedChat(mc.thePlayer, "key.dss.toggletpm", StatCollector.translateToLocal(Config.toggleTargetPassiveMobs() ? "key.dss.enable" : "key.dss.disable"));
-				} else {
-					PlayerUtils.sendTranslatedChat(mc.thePlayer, "key.dss.toggleat", StatCollector.translateToLocal(Config.toggleAutoTarget() ? "key.dss.enable" : "key.dss.disable"));
-				}
 			} else if (kb == keys[KEY_SKILLS_GUI].getKeyCode()) {
 				PacketDispatcher.sendToServer(new OpenGuiPacket(CommonProxy.GUI_SKILLS));
-			}  else if (kb == keys[KEY_TOGGLE_HUD].getKeyCode()) {
-				Config.comboHudEnabled = !Config.comboHudEnabled;
-				PlayerUtils.sendTranslatedChat(mc.thePlayer, "key.dss.togglehud", StatCollector.translateToLocal(Config.comboHudEnabled ? "key.dss.enable" : "key.dss.disable"));
 			} else {
 				handleTargetingKeys(mc, kb, skills);
 			}
