da23529318f2e330e459d830c34e40dd7984bfdd
coolAlias <BASbliznet@gmail.com>
2019-12-01 01:40:20 -0800

    Updated ArmorBreak class documentation

--- src/main/java/dynamicswordskills/skills/ArmorBreak.java
+++ src/main/java/dynamicswordskills/skills/ArmorBreak.java
@@ -168,15 +168,6 @@ public class ArmorBreak extends SkillActive
 		return (DSSKeyHandler.keys[DSSKeyHandler.KEY_ATTACK].isKeyDown() || (Config.allowVanillaControls() && Minecraft.getMinecraft().gameSettings.keyBindAttack.isKeyDown()));
 	}
 
-	/**
-	 * ArmorBreak's activation was triggered from the client side and it will be over
-	 * on the server by the time the client receives the packet, so don't bother
-	 */
-	@Override
-	protected boolean sendClientUpdate() {
-		return false;
-	}
-
 	@Override
 	protected boolean onActivated(World world, EntityPlayer player) {
 		activeTimer = 1; // needs to be active for hurt event to process correctly
