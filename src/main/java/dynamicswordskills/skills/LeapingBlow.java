e9b41944036c775f99e3975aa6349c75b46070fe
coolAlias <BASbliznet@gmail.com>
2019-12-05 21:06:11 -0800

    Fixed Leaping Blow unsetting right-click when item not in use
    This allows players to activate multiple times in a row without having to release right-click so long as they are not holding e.g. a shield.

--- src/main/java/dynamicswordskills/skills/LeapingBlow.java
+++ src/main/java/dynamicswordskills/skills/LeapingBlow.java
@@ -140,8 +140,10 @@ public class LeapingBlow extends SkillActive
 		if (key == mc.gameSettings.keyBindJump) {
 			if (player.onGround && PlayerUtils.isBlocking(player) && canUse(player)) {
 				ticksTilFail = 10;
-				KeyBinding.setKeyBindState(mc.gameSettings.keyBindUseItem.getKeyCode(), false);
-				KeyBinding.setKeyBindState(DSSKeyHandler.keys[DSSKeyHandler.KEY_BLOCK].getKeyCode(), false);
+				if (player.isUsingItem()) {
+					KeyBinding.setKeyBindState(mc.gameSettings.keyBindUseItem.getKeyCode(), false);
+					KeyBinding.setKeyBindState(DSSKeyHandler.keys[DSSKeyHandler.KEY_BLOCK].getKeyCode(), false);
+				}
 				return true;
 			}
 		} else if (canExecute(player) && activate(player)) {
