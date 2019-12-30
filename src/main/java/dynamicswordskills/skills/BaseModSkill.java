8b224439994d4c046e2be823bea14d839e22769f
coolAlias <BASbliznet@gmail.com>
2019-12-30 02:09:41 -0800

    Added Item-independent icon location to skill class, for issue #30

--- src/main/java/dynamicswordskills/skills/BaseModSkill.java
+++ src/main/java/dynamicswordskills/skills/BaseModSkill.java
@@ -1,13 +1,20 @@
 package dynamicswordskills.skills;
 
+import dynamicswordskills.ref.ModInfo;
+
 public abstract class BaseModSkill extends SkillActive
 {
+	/** Base directory for skill icons */
+	public static final String ICON_LOCATION = ModInfo.ID + ":textures/skills/";
+
 	/**
-	 * Base active mod skill with standard tooltip entry
+	 * Base active mod skill with standard tooltip entry and icon location
+	 * @param translationKey Assumed to match the icon file name; if that is not the case, call {@link #setIconLocation(String)} separately
 	 */
 	public BaseModSkill(String translationKey) {
 		super(translationKey);
 		this.addTranslatedTooltip(getTranslationKey() + ".tooltip");
+		this.setIconLocation(ICON_LOCATION + translationKey + ".png");
 	}
 
 	public BaseModSkill(SkillActive skill) {
--- src/main/java/dynamicswordskills/skills/SkillBase.java
+++ src/main/java/dynamicswordskills/skills/SkillBase.java
@@ -58,6 +58,12 @@ public abstract class SkillBase
 	/** Unique ResourceLocation for this skill */
 	private ResourceLocation registryName = null;
 
+	/** Placeholder skill icon used by default */
+	private static final ResourceLocation DEFAULT_ICON = new ResourceLocation(ModInfo.ID, "textures/skills/default.png");
+
+	/** Icon texture location, if any */
+	private ResourceLocation iconLocation = null;
+
 	/** Language registry translation key */
 	public final String translationKey;
 
@@ -81,9 +87,10 @@ public abstract class SkillBase
 	 * Copy constructor creates a level zero version of the skill
 	 */
 	protected SkillBase(SkillBase skill) {
+		this.id = skill.id;
+		this.iconLocation = skill.iconLocation;
 		this.registryName = skill.registryName;
 		this.translationKey = skill.translationKey;
-		this.id = skill.id;
 		this.tooltip.addAll(skill.tooltip);
 	}
 
@@ -205,6 +212,17 @@ public abstract class SkillBase
 	 */
 	public boolean displayInGroup(SkillGroup group) {
 		return this.getRegistryName() != null && group.label.equalsIgnoreCase(this.getRegistryName().getResourceDomain());
+	}
+
+	/** Return the texture path for the skill's icon */
+	public ResourceLocation getIconLocation() {
+		return (iconLocation == null ? DEFAULT_ICON : iconLocation);
+	}
+
+	/** Sets the texture path for the skill's icon */
+	public SkillBase setIconLocation(String location) {
+		this.iconLocation = DynamicSwordSkills.getResourceLocation(location);
+		return this;
 	}
 
 	/** Each skill's ID can be used as a key to retrieve it from the map */
