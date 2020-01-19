1b783642a5ba41f7600196ba0ca4ff1b49a8b87f
coolAlias <BASbliznet@gmail.com>
2020-01-19 00:39:18 -0800

    Fixed activateTargetingSkill continuing to activate targeting skills beyond the first

--- src/main/java/dynamicswordskills/entity/DSSPlayerInfo.java
+++ src/main/java/dynamicswordskills/entity/DSSPlayerInfo.java
@@ -595,8 +595,8 @@ public class DSSPlayerInfo implements IExtendedEntityProperties
 		for (SkillBase skill : SkillRegistry.getValues()) {
 			if (skill instanceof ILockOnTarget && skill instanceof SkillActive) {
 				SkillBase instance = getPlayerSkill(skill);
-				if (instance != null) {
-					((SkillActive) instance).activate(player);
+				if (instance != null && ((SkillActive) instance).activate(player)) {
+					break;
 				}
 			}
 		}
